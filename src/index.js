6edf0fc922c47afd114c9ea3c561a9360f5fb2d6
Chahid <80324892+chahidy@users.noreply.github.com>
2022-07-14 20:48:27 +0200

    changed from ReactDom to createRoot

--- src/index.js
+++ src/index.js
@@ -14,18 +14,17 @@ Coded by www.creative-tim.com
 */
 
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "App";
 
 // Soft UI Dashboard React Context Provider
 import { SoftUIControllerProvider } from "context";
 
-ReactDOM.render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <SoftUIControllerProvider>
       <App />
     </SoftUIControllerProvider>
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
